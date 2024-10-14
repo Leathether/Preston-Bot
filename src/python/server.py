fb9d1a03a8686a2f20046327fd4e5c2a1e656d6c
Peter Magenheim <pwmagenheim@gmail.com>
2024-10-14 18:54:28 -0400

    Got the embeddings to hook up with the javascript

--- src/python/server.py
+++ src/python/server.py
@@ -1,17 +1,26 @@
 # importing flask to give the flask to the user.
 from flask import Flask, jsonify
-from embeddings import embeddings
+from flask_cors import CORS
+from embeddings import embeddings, text
 
 
 # app instance
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/api/home", methods=["GET"])
-def return_home():
+
+@app.route("/api/embeddings", methods=["GET"])
+def return_embeddings():
     return jsonify({
         'pretraining': embeddings
     })
 
+@app.route("/api/text", methods=["GET"])
+def return_text():
+    return jsonify({
+        'text': text 
+    })
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
